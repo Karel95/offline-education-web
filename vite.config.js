11f94efe965f24eec861ef2356a86832908fc924
Karel <karelh2207@gmail.com>
2024-10-08 15:28:13 -0300

    evolving into PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,48 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['suriname-logo.png'],
+      manifest: {
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        lang: 'en',
+        name: 'Suriname Offline Education Web | By KarlDev',
+        short_name: 'EduWeb',
+        description:
+          'PWA for offline access to educational content in regions with limited internet.',
+        theme_color: '#ffffff',
+        background_color: '#000000',
+        screenshots: [
+          {
+            src: '/img/screenshots/desktop-1366x769.png',
+            sizes: '1366x769',
+            type: 'image/png',
+            form_factor: 'wide'
+          },
+          {
+            src: '/img/screenshots/mobile-247x549.png',
+            sizes: '247x549',
+            type: 'image/png',
+            form_factor: 'narrow'
+          }
+        ],
+        icons: [
+          {
+            src: '/img/suriname-logo.png',
+            sizes: 'any',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: '@', replacement: '/src' }],
   },
 });
