cdbd0f26dddca02db190fedfd666fe0712b29e8a
Karel <karelh2207@gmail.com>
2024-10-21 14:17:27 -0300

    updating

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -10,38 +10,8 @@ import React, { useState, Component } from 'react';
 import { NetlifyForm, Honeypot } from 'react-netlify-forms';
 
 const Form = () => {
-  // const [formData, setFormData] = useState({
-  //   fullName: '',
-  //   email: '',
-  //   message: '',
-  //   agreed: false,
-  // });
-
-  // const handleChange = (e) => {
-  //   const { id, value, type, checked } = e.target;
-  //   setFormData({
-  //     ...formData,
-  //     [id]: type === 'checkbox' ? checked : value,
-  //   });
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const form = e.target;
-
-  //   const data = new FormData(form);
-
-  //   fetch('/', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //     body: new URLSearchParams(data).toString(),
-  //   })
-  //     .then(() => alert('Formulario enviado con éxito'))
-  //     .catch((error) => alert('Error al enviar el formulario', error));
-  // };
-
   return (
-    <NetlifyForm name="Contact" honeypotName="bot-field">
+    <NetlifyForm name="Contact" action='/thanks' netlify honeypotName="bot-field">
       {({ handleChange, success, error }) => (
         <div className="mx-auto lg:w-6/12">
           <Honeypot />
@@ -56,7 +26,6 @@ const Form = () => {
               Complete this form and we will get back to you in 24 hours.
             </PageTitle>
             <div className="mb-8 gap-8">
-              {/* <label htmlFor="name">Name:</label> */}
               <Input
                 type="text"
                 name="name"
@@ -66,7 +35,6 @@ const Form = () => {
                 label="Full Name"
                 onChange={handleChange}
               />
-              {/* <label htmlFor="name">Email:</label> */}
               <Input
                 type="email"
                 name="email"
@@ -78,7 +46,6 @@ const Form = () => {
               />
             </div>
             <div>
-              {/* <label htmlFor="message">Message:</label> */}
               <Textarea
                 type="text"
                 name="message"
@@ -105,100 +72,6 @@ const Form = () => {
         </div>
       )}
     </NetlifyForm>
-    // <>
-    //   <PageTitle section="Contact Us" heading="Want to work with us?">
-    //     Complete this form and we will get back to you in 24 hours.
-    //   </PageTitle>
-    //   <form
-    //     className="mx-auto w-full mt-12 lg:w-5/12"
-    //     name="contact"
-    //     method="POST"
-    //     data-netlify="true"
-    //     data-netlify-honeypot="bot-field"
-    //     onSubmit={handleSubmit}
-    //   >
-    //     {/* Campo oculto requerido por Netlify */}
-    //     <input type="hidden" name="form-name" value="contact" />
-
-    //     {/* Campo honeypot oculto para evitar bots */}
-    //     <p className="hidden">
-    //       <label>
-    //         Don’t fill this out if you’re human: <input name="bot-field" />
-    //       </label>
-    //     </p>
-
-    //     <div className="mb-8 flex gap-8">
-    //       <label htmlFor="fullName">
-    //         <Input
-    //           id="fullName"
-    //           name="fullName"
-    //           type="text"
-    //           variant="outlined"
-    //           size="lg"
-    //           label="Full Name"
-    //           value={formData.fullName}
-    //           onChange={handleChange}
-    //           required
-    //         />
-    //       </label>
-    //       <label htmlFor="email">
-    //         <Input
-    //           id="email"
-    //           name="email"
-    //           type="email"
-    //           variant="outlined"
-    //           size="lg"
-    //           label="Email Address"
-    //           value={formData.email}
-    //           onChange={handleChange}
-    //           required
-    //         />
-    //       </label>
-    //     </div>
-    //     <Textarea
-    //       id="message"
-    //       name="message"
-    //       variant="outlined"
-    //       size="lg"
-    //       label="Message"
-    //       rows={8}
-    //       value={formData.message}
-    //       onChange={handleChange}
-    //       required
-    //     />
-    //     <Checkbox
-    //       id="agreed"
-    //       name="agreed"
-    //       checked={formData.agreed}
-    //       onChange={handleChange}
-    //       label={
-    //         <Typography
-    //           variant="small"
-    //           color="gray"
-    //           className="flex items-center font-normal"
-    //         >
-    //           I agree the
-    //           <a
-    //             href=""
-    //             className="font-medium transition-colors hover:text-gray-900"
-    //           >
-    //             &nbsp;Terms and Conditions
-    //           </a>
-    //         </Typography>
-    //       }
-    //       containerProps={{ className: '-ml-2.5' }}
-    //     />
-    //     <Button
-    //       variant="gradient"
-    //       size="lg"
-    //       className="mt-8"
-    //       fullWidth
-    //       type="submit"
-    //     >
-    //       Send Message
-    //     </Button>
-    //   </form>
-    // </>
   );
 };
 
