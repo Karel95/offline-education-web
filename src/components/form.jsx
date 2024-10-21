ee31860f527add5ac45e23a0dce71704c70b73be
Karel <karelh2207@gmail.com>
2024-10-21 14:31:41 -0300

    updating

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -6,14 +6,19 @@ import {
   Textarea,
   Checkbox,
 } from '@material-tailwind/react';
-import React, { useState, Component } from 'react';
+import React from 'react';
 import { NetlifyForm, Honeypot } from 'react-netlify-forms';
 
 const Form = () => {
   return (
-    <NetlifyForm name="Contact" action='/thanks' netlify honeypotName="bot-field">
+    <NetlifyForm
+      name="Contact"
+      action="/thanks"
+      netlify
+      honeypotName="bot-field"
+    >
       {({ handleChange, success, error }) => (
-        <div className="mx-auto lg:w-6/12">
+        <>
           <Honeypot />
           {success && <p>Thanks for contacting us!</p>}
           {error && (
@@ -21,55 +26,56 @@ const Form = () => {
               Sorry, we could not reach our servers. Please try again later.
             </p>
           )}
-          <div className="w-full mt-12 lg:w-12/12">
-            <PageTitle section="Contact Us" heading="Want to work with us?">
-              Complete this form and we will get back to you in 24 hours.
-            </PageTitle>
-            <div className="mb-8 gap-8">
-              <Input
-                type="text"
-                name="name"
-                id="name"
-                variant="outlined"
-                size="lg"
-                label="Full Name"
-                onChange={handleChange}
-              />
-              <Input
-                type="email"
-                name="email"
-                id="email"
-                variant="outlined"
-                size="lg"
-                label="Email Address"
-                onChange={handleChange}
-              />
+          <div className="mx-auto lg:w-6/12">
+            <div className="w-full mt-12 lg:w-12/12">
+              <PageTitle section="Contact Us" heading="Want to work with us?">
+                Complete this form and we will get back to you in 24 hours.
+              </PageTitle>
+              <div className="mb-8 gap-8">
+                <Input
+                  type="text"
+                  name="name"
+                  id="name"
+                  variant="outlined"
+                  size="lg"
+                  label="Full Name"
+                  onChange={handleChange}
+                />
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  variant="outlined"
+                  size="lg"
+                  label="Email Address"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <Textarea
+                  name="message"
+                  id="message"
+                  variant="outlined"
+                  size="lg"
+                  label="Message"
+                  rows="4"
+                  onChange={handleChange}
+                />
+              </div>
             </div>
             <div>
-              <Textarea
-                type="text"
-                name="message"
-                id="message"
-                variant="outlined"
+              <Button
+                variant="gradient"
                 size="lg"
-                label="Message"
-                rows="4"
-                onChange={handleChange}
-              />
+                className="mt-8"
+                fullWidth
+                type="submit"
+              >
+                Submit
+              </Button>
             </div>
           </div>
-          <div>
-            <Button
-              variant="gradient"
-              size="lg"
-              className="mt-8"
-              fullWidth
-              type="submit"
-            >
-              Submit
-            </Button>
-          </div>
-        </div>
+        </>
       )}
     </NetlifyForm>
   );
